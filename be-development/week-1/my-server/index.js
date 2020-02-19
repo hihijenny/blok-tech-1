e0f98ddadb8877fd01c9cea0affefcd0713cc024
hihijenny <jenny_nijhof@hotmail.com>
2020-02-19 13:28:34 +0100

    404 pagina toegevoegd

--- be-development/week-1/my-server/index.js
+++ be-development/week-1/my-server/index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  app.use(express.static("public"));
-})
+app.use(express.static('public'))
 
-app.get('/about', (req, res) => {
-  res.send("hoi");
-})
+// app.get('/', (req, res) => {
+//   app.use(express.static("public"));
+// })
 
+// app.get('/about', (req, res) => {
+//   res.send("hoi");
+// })
+
+// app.get('/index', (req, res) => {
+//   res.send("index");
+// });
+
+//404 error
+app.use(function(req, res) {
+  res.send("404: Page not found", 404);
+});
 app.listen(port, () => console.log(`running on port ${port}`))
